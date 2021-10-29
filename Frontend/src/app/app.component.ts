b1e84410e2bb1369bef934166b611a2498fdf302
Miguel Ruiz <miguel.ruizhz@uanl.edu.mx>
2021-10-29 17:09:24 -0500

    PANEL MENU PRINCIPAL

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { MenuController } from '@ionic/angular';
+
+import { Componente } from './interfaces/interfaces';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+
+ // Se va a estar emitiendo valores de componente como un arreglo
+ componentes: Observable<Componente[]>;
+
+ constructor( private menuCtrl: MenuController,
+              private dataService: DataService) {}
+
+ ngOnInit() {
+   this.componentes = this.dataService.getMenuOpts();
+ }
+
 }
